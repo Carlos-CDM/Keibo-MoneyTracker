0bd1c8b6c5d7a18cb1160f6d5b1bb435ca0cfa90
Carlos-CDM <gamx256@hotmail.com>
2021-01-16 22:21:30 +0100

    Size of balance lines updates

--- keibomoneytracker/gui_keibomoneytracker.cpp
+++ keibomoneytracker/gui_keibomoneytracker.cpp
@@ -2234,12 +2234,12 @@ void Gui_KeiboMoneyTracker::displayArticlesOnTable()
     if (currentAccount.getBalanceInMonth(currentMonth) >= abs(10000.0)  ){
         ui->lineMonthlyBalance->setFixedWidth(90);
     } else {
-        ui->lineMonthlyBalance->setFixedWidth(70);
+        ui->lineMonthlyBalance->setFixedWidth(82);
     }
     if (currentAccount.getBalanceInYear() >= abs(10000.0)  ){
         ui->lineYearlyBalance->setFixedWidth(90);
     } else {
-        ui->lineYearlyBalance->setFixedWidth(70);
+        ui->lineYearlyBalance->setFixedWidth(82);
     }
 
     ui->tableWidget->setColumnCount(4);
